73a556e5f8a2e7f2d79404a37faad4b130591a59
Nur <noorin.sa.99@gmail.com>
2023-03-23 10:42:57 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+// has a function expression called divide
+const divide = (a, b) => a / b;
+
+// has an arrow function called square
+const square = (x) => x * x;
+
+// has an arrow function called add
+const add = (a, b) => a + b;
